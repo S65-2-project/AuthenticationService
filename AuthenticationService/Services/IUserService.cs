dbd4e8d765efb843ebcbb94befec74afeea2fcf3
Bstijn <baltessen.stijn@gmail.com>
2020-04-17 11:51:57 +0200

    s65-97: started tests

--- AuthenticationService/Services/IUserService.cs
+++ AuthenticationService/Services/IUserService.cs
@@ -6,6 +6,13 @@ namespace AuthenticationService.Services
 {
     public interface IUserService
     {
+        /// <summary>
+        /// Hashes password and saves this to the database
+        /// </summary>
+        /// <param name="id"></param>
+        /// <param name="email"></param>
+        /// <param name="password">unhashed password</param>
+        /// <returns>nothing</returns>
         Task InsertRegisteredUser(Guid id, string email, string password);
     }
 }
