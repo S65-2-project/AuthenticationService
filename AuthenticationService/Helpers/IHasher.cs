840fc9582d3d65a1cbe6f09489b20e76b25484e2
Wouter Vandenboorn <wouter04316@gmail.com>
2020-04-17 10:20:28 +0200

    updated added hashing , needs to be tested

---
+++ AuthenticationService/Helpers/IHasher.cs
@@ -0,0 +1,13 @@
+﻿using System.Threading.Tasks;
+
+namespace UserService.Helpers
+{
+    public interface IHasher
+    {
+        Task<byte[]> HashPassword(string password, byte[] salt);
+
+        byte[] CreateSalt();
+
+        Task<bool> VerifyHash(string password, byte[] salt, byte[] hash);
+    }
+}
