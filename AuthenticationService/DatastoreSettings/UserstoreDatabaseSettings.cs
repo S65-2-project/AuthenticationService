77924af6569c40afa275aaa0e6cdb97bc9ef1a1b
Wouter Vandenboorn <wouter04316@gmail.com>
2020-04-17 09:35:00 +0200

    Added user file

--- AuthenticationService/DatastoreSettings/UserstoreDatabaseSettings.cs
+++ AuthenticationService/DatastoreSettings/UserstoreDatabaseSettings.cs
@@ -2,6 +2,15 @@
 {
     public class UserstoreDatabaseSettings
     {
-        
+        public string UserCollectionName { get; set; }
+        public string ConnectionString { get; set; }
+        public string DatabaseName { get; set; }
+    }
+    
+    public interface IUserstoreDatabaseSettings
+    {
+        string UserCollectionName { get; set; }
+        string ConnectionString { get; set; }
+        string DatabaseName { get; set; }
     }
 }
--- AuthenticationService/Repositories/UserRepository.cs
+++ AuthenticationService/Repositories/UserRepository.cs
@@ -1,7 +1,9 @@
-﻿namespace AuthenticationService.Repositories
+﻿using MongoDB.Driver;
+
+namespace AuthenticationService.Repositories
 {
     public class UserRepository
     {
-        
+        private readonly IMongoCollection<User> _users;
     }
 }
